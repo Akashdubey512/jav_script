f8b9184e0d353321f7bb4cee2cdf64a675462550
Akashdubey512 <yogesh8853599651@gmail.com>
2025-06-09 05:00:28 +0000

    container config done

--- 1.js
+++ 1.js
@@ -1,3 +1,26 @@
+console.log("hi")
 const course = {
-    coursename: "js in hindi"
+    coursename : "javascript",
+    student : "Akash"
 }
+console.log (course.coursename)
+// destructuring of objects
+// const {coursename} = course
+// console.log (coursename)
+
+const {coursename: cname } = course
+console.log (cname)
+// JSON api
+// {
+//     "student": "Akash",
+//     "course": "javqscript"
+
+// }
+//      or in the form of array of objects
+// [
+//     {},
+//     {},
+//     {}
+// ]     
+
+
