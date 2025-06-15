ed909da686fb070a0bbeaae77b3052abb1f1eb82
Akashdubey512 <yogesh8853599651@gmail.com>
2025-06-15 12:42:11 +0000

    Asyn javascript done

---
+++ project_6_key/index.js
@@ -0,0 +1,17 @@
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', function (e) {
+    insert.innerHTML = `
+    <div class="color"><table>
+        <tr>
+            <td>Key</td>
+            <td>Keycode </td>
+            <td>Code</td>
+        </tr>
+        <tr>
+            <td>${e.key === " " ? 'space' : e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+    </table></div>`;
+});
